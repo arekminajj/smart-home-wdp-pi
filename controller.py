cc4768b06d041ecf8cc4cc1f48fd29d24b4198d5
Arkadiusz Cios <arek123113@gmail.com>
2023-10-23 18:26:39 +0100

    FIX: typo in json key

--- controller.py
+++ controller.py
@@ -5,7 +5,7 @@ from time import sleep
 while(True):
     data = apiClient.getData()
     temp = data['temp']
-    temp_setting = data['temp-set']
+    temp_setting = data['temp_set']
     light = data['light_1']
     lcd.update_lcd(f"Temperature: {temp}C\nTemp. set.: {temp_setting}C")
     sleep(10)
