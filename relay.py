0638c8742167f950d7fb95c89b26365b76935f86
Arkadiusz Cios <arek123113@gmail.com>
2023-10-24 15:22:35 +0100

    FIX: relay turnig opposite values

--- relay.py
+++ relay.py
@@ -6,8 +6,8 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(relay_ch, GPIO.OUT)
 
-def low():
-	GPIO.output(relay_ch, GPIO.LOW)
 def high():
+	GPIO.output(relay_ch, GPIO.LOW)
+def low():
 	GPIO.output(relay_ch, GPIO.HIGH)
 #GPIO.cleanup()
