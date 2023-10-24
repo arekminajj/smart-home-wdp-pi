de48d016143552903d4e99904b3890e046f4ce8b
Arek Cios <arek123113@gmail.com>
2023-10-24 16:27:00 +0200

    delete junk from controller.py

--- controller.py
+++ controller.py
@@ -3,17 +3,6 @@ import apiClient
 from time import sleep
 import relay
 import temperature
-#from gpiozero import Button
-#from signal import pause
-
-#screen_setting = 0
-#0 znaczy temperatura
-#1 znaczy swiatlo
-
-#button = Button(2)
-
-
-#button.when_pressed = change_screen
 
 while(True):
     data = apiClient.getData()
@@ -24,7 +13,6 @@ while(True):
     light = int(light)
     lcd.update_lcd(f"Temperatura: {temp}C\nTemp. set.: {temp_setting}C")
     sleep(3)
-
 
     if light == 1:
         relay.high()
